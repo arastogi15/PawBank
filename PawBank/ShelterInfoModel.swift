b2d38c6f83786abf754cf8940d958fb4a2ec4c46
Natalie Mackraz <mackraz@usc.edu>
2017-02-18 12:23:46 -0800

    MVC format and more UI elements added

---
+++ PawBank/ShelterInfoModel.swift
@@ -0,0 +1,13 @@
+//
+//  ShelterInfoModel.swift
+//  PawBank
+//
+//  Created by Natalie Mackraz on 2/18/17.
+//  Copyright © 2017 Natalie Mackraz. All rights reserved.
+//
+
+import UIKit
+
+class ShelterInfoModel: NSObject {
+
+}
